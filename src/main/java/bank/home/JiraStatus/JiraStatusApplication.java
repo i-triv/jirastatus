e5ff29810ebc4302aadc2414cc150f2e82dca992
triv <to@triv.ru>
2023-10-25 20:27:55 +0300

    fist commit, for maked project, empty spring template

---
+++ src/main/java/bank/home/JiraStatus/JiraStatusApplication.java
@@ -0,0 +1,13 @@
+package bank.home.JiraStatus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiraStatusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JiraStatusApplication.class, args);
+	}
+
+}
